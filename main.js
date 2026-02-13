58ddd0485e30881f8a39875800d1049bb100550b
gelie <https://github.com/geliemadason/Xaviabot9.git>
2026-02-13 17:09:21 +0800

    update main.js

--- main.js
+++ main.js
@@ -15,15 +15,15 @@ window.onload = () => {
     bgAudio.play()
       .then(() => {
 
-        // itago ang music button
-        startAudio.style.display = 'none';
 
-        // existing mo
+   startAudio.style.display = 'none';
+
+
         setTimeout(() => {
           document.body.classList.remove("not-loaded");
         }, 1000);
 
-        // 🔥 romantic reveal after 3 seconds
+
         setTimeout(() => {
           message.classList.add("show-romantic");
 
@@ -31,7 +31,7 @@ window.onload = () => {
           buttonYes.classList.add("show-romantic");
           buttonNo.classList.add("show-romantic");
 
-        // add soft glow to Yes button
+      
          buttonYes.classList.add("glow");
 
        }, 800);
@@ -50,7 +50,7 @@ window.onload = () => {
   
   
 
-// function para tumakbo ang No button
+
 function moveNoButton() {
   let x = Math.random() * (window.innerWidth - buttonNo.offsetWidth);
   let y = Math.random() * (window.innerHeight - buttonNo.offsetHeight);
@@ -60,22 +60,19 @@ function moveNoButton() {
   buttonNo.style.top = y + "px";
 }
 
-// pag hover
+
 buttonNo.addEventListener("mouseenter", moveNoButton);
 
-// pag click (extra protection 😈)
 buttonNo.addEventListener("click", function(e) {
   e.preventDefault();
   moveNoButton();
 });
 
   
-  // 🎲 Random messages
 const randomMessages = [
-  "Hehe nag yes siya, what time po? chat ni raniel sa IG kung what time",
+  "Hehe nag yes siya, what time po? chat lang sa IG kung what time",
   "Sure naka ha? Wala nay bawian kay nag yes naka!😏",
   "Ikaw rajud akong Valentina🫶🌸",
-  "Excited nako makita ka, ngano bitaw nag yes HAHAHA😌",
   "Promise mag enjoy ta ani kay e treat man tikag right💖",
   "Feb 14 alas 4am sa kadlawn atong laag, indot ng sayo kay dakog agi🤭"
 ];
@@ -85,15 +82,14 @@ const randomMessages = [
   buttonYes.addEventListener('click', () => {
   bgAudio.volume = 0.2;
 
-  // 🎲 random message
+
   const randomIndex = Math.floor(Math.random() * randomMessages.length);
   document.querySelector('#customAlert p').textContent = randomMessages[randomIndex];
 
-  // show custom modal
+
   const customAlert = document.getElementById('customAlert');
   customAlert.classList.add('show');
 
-  // close button
   const closeAlert = document.getElementById('closeAlert');
   closeAlert.onclick = () => {
     customAlert.classList.remove('show');
